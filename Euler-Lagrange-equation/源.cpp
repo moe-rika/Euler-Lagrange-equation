c2ed59dc170234a699e0ca0cb2be073387651a75
DiazoPlus <wwwxxttii@gmail.com>
2020-01-17 20:13:39 +0800

    C

--- Euler-Lagrange-equation/源.cpp
+++ Euler-Lagrange-equation/源.cpp
@@ -1,20 +1,25 @@
+#define  _CRT_SECURE_NO_WARNINGS 
+
+
+
+
 #include <array>
 #include <math.h>
 #include <cstdio>
 
-template <unsigned int Dim,typename DataType>
+template <unsigned int Dim, typename DataType>
 class Lagrangian
 {
 public:
 	Lagrangian(std::array<DataType, Dim> _position, std::array<DataType, Dim> _velocity) :position(_position), velocity(_velocity) {};
 	~Lagrangian() = default;
-	
+
 
 	std::array<DataType, Dim> position;
 	std::array<DataType, Dim> velocity;
-	
+
 	virtual DataType calc_lagrangian() { return DataType(0); };
-	
+
 	void update_p()
 	{
 		for (size_t i = 0; i < Dim; i++)
@@ -63,7 +68,7 @@ private:
 		return (a - 2 * b + c) / delta / delta;
 	}
 
-	void find_v(DataType target_result,int i)
+	void find_v(DataType target_result, int i)
 	{
 		while (true)
 		{
@@ -74,32 +79,37 @@ private:
 				break;
 		}
 	}
-	const DataType delta = 10E-7;
+	const DataType delta = 10E-5;
 	const DataType precise = 10E-4;
-	const DataType time_q = 10E-3;
+	const DataType time_q = 10E-4;
 };
 
-class MyClass : public Lagrangian<1,double>
+class MyClass : public Lagrangian<2, double>
 {
 public:
-	MyClass(std::array<double, 1> _position, std::array<double, 1> _velocity) :Lagrangian(_position, _velocity) {};
+	MyClass(std::array<double, 2> _position, std::array<double, 2> _velocity) :Lagrangian(_position, _velocity) {};
 	~MyClass() = default;
 	const double L = 1;
 	const double m = 1;
 	const double g = 9.8;
 	double calc_lagrangian() override {
-		double T = m * L * velocity[0] * L*velocity[0] / 2;
-		double V = -L * m * g * sin(position[0]);
-		return T - V; 
+		return 1 / 6. * m * L * L * (\
+			velocity[1] * velocity[1] + 4 * velocity[0] * velocity[0]\
+			+ 3 * velocity[0] * velocity[1] * cos(position[0] - position[1]) \
+			) \
+			+ 1 / 2.*m*g*L*(3 * cos(position[0]) + cos(position[1]));
 	};
 };
 
+
 int main()
 {
-	MyClass m({ 0 }, { 0 });
-	for (size_t i = 0; i < 10000; i++)
-	{	
-		printf("%f , %f\n",m.position[0],m.velocity[0]);
+	MyClass m({ 0.5, -0.8 }, { 0,0 });
+	FILE* pFile = fopen("E:\\test.txt", "w");
+	for (size_t i = 0; i < 100000; i++)
+	{
+		if (i % 100 == 0)
+			fprintf(pFile,"%f %f\n", m.position[0], m.position[1]);
 		m.update_p();
 		m.update_v();
 	}
